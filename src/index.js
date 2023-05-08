f928bc1b5fd045a6ed56e6b57fa1c5bd3fa778e7
Jiachun Xiang <max997123@gmail.com>
2023-05-07 22:44:52 -0400

    add styles

--- src/index.js
+++ src/index.js
@@ -1,30 +1,26 @@
-import tasks from './tasks.js';
 import './style.css';
 import './storage.js';
-import { addTask, deleteTask } from './app.js';
-import { updateStatus, clearCompleted } from './update.js';
-
-const tasks = [
-  { description: 'Name', score: 100, index: 1 },
-  { description: 'Name', score: 20, index: 2 },
-  { description: 'Name', score: 50, index: 3 },
-  { description: 'Name', score: 78, index: 4 },
-  { description: 'Name', score: 125, index: 5 },
-  { description: 'Name', score: 77, index: 6 },
-  { description: 'Name', score: 42, index: 7 },
+const scores = [
+  { description: 'Name', point: 100, index: 1 },
+  { description: 'Name', point: 20, index: 2 },
+  { description: 'Name', point: 50, index: 3 },
+  { description: 'Name', point: 78, index: 4 },
+  { description: 'Name', point: 125, index: 5 },
+  { description: 'Name', point: 77, index: 6 },
+  { description: 'Name', point: 42, index: 7 },
 ];
 
-function renderTasks() {
-  const todoList = document.getElementById('todo-list');
+function renderScores() {
+  const scoreList = document.getElementById('scoreList');
 
-  tasks.sort((a, b) => a.index - b.index);
+  scores.sort((a, b) => a.index - b.index);
 
-  tasks.forEach((task) => {
-    const taskItem = document.createElement('li');
-    taskItem.textContent = task.description;
+  scores.forEach((score) => {
+    const newScore = document.createElement('li');
+    newScore.innerHTML = `${score.description}:${score.point}`;
 
-    todoList.appendChild(taskItem);
+    scoreList.appendChild(newScore);
   });
 }
 
-renderTasks();
+renderScores();

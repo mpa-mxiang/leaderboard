e7016170dd2bf875769b39acb6d8099b62c450d8
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-05-15 19:14:21 -0400

    create GET functions

--- src/api.js
+++ src/api.js
@@ -4,21 +4,29 @@ const URL = BASE_URL + POST_ID + '/scores/';
 
 
 // POST action: get scores from users
-export const postScore=async(data)=> {
-    try {
-      const response = await fetch(URL, {
-        method: "POST", // or 'PUT'
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
-  
-      const result = await response.json();
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  }
-  
-// GET action: get scores from API
+export const postScore = async (data) => {
+  try {
+    const response = await fetch(URL, {
+      method: "POST", 
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
 
+    const result = await response.json();
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+// GET action: get scores from API
+export const getScores = async () => {
+  try {
+    const response = await fetch(URL);
+    const result = await response.json();
+    return result.result;
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './style.css';
-import './api';
+import { postScore, getScore } from './api';
 
 const scores = [
   { name: 'Name', point: 100, index: 1 },
@@ -25,3 +25,42 @@ function renderScores() {
 }
 
 renderScores();
+
+const createScores=()=>{
+  const form = document.getElementById('list-form');
+  const formName = document.getElementById('name');
+  const formScore = document.getElementById('score');
+  form.addEventListener('submit',(event)=>{
+    event.preventDefault();
+    const newName = formName.value;
+    const newScore = Number(formScore.value);
+    console.log(postScore);
+    const score = {
+      user: newName,
+      score: newScore,
+    }
+    postScore(score);
+    refreshScores();
+  });
+
+}
+
+createScores();
+
+const refreshScores = async () => {
+  const scoreList = document.getElementById('scoreList');
+  try {
+    const scores = await getScores();
+    scoreList.innerHTML = '';
+    scores.forEach(score => {
+      const scoreItem = document.createElement('li');
+      scoreItem.innerText = `${score.user}: ${score.score}`;
+      scoreList.appendChild(scoreItem);
+    });
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+const refreshButton = document.getElementById('refresh');
+refreshButton.addEventListener('click', refreshScores);
